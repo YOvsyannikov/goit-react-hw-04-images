eb237aa181daa51f7124c2fa2ffd61425450d634
Yuriy Ovsyannikov <yovsyannikov@gmail.com>
2022-10-12 16:19:13 +0300

    some changes 3

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiService = async (query, page) => {
   const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=18963132-a5d8582da162d4c0f5ee62384&image_type=photo&orientation=horizontal&per_page=12`,
+    `https://pixabay.com/api/?q=${query}&page=${page}&key=29560891-792771529aa052fb706988eec&image_type=photo&orientation=horizontal&per_page=12`
   );
   return data.hits;
 };
